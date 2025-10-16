a1fa26cfcb9ede9b0a9f5eeb23c1fd18a7c04f88
Kristian Cybul <kcybul123@gmail.com>
2025-10-16 12:27:31 -0500

    Added /cmds command and additional info

--- discord-bot/deploy-commands.js
+++ discord-bot/deploy-commands.js
@@ -10,11 +10,11 @@ const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
 	// Grab all the command files from the commands directory you created earlier
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+	const commandPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandPath).filter((file) => file.endsWith('.js'));
 	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
+		const filePath = path.join(commandPath, file);
 		const command = require(filePath);
 		if ('data' in command && 'execute' in command) {
 			commands.push(command.data.toJSON());
