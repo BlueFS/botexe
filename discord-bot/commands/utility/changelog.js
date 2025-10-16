6cdcaa03afbe24421ff96ea8cb4ff5eb3020e7ba
Sirbumpkis <boonemcconathy7@gmail.com>
2025-10-16 14:36:23 -0400

    Added changelog command changelog.js

--- discord-bot/commands/utility/changelog.js
+++ discord-bot/commands/utility/changelog.js
@@ -1,1 +1,10 @@
-#
+const { SlashCommandBuilder} = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('Changelog')
+        .setDescription('What has changes have been made'),
+    async execute(interaction) {
+        await interaction.reply('There is none right now. Bot Version: v1.1');
+    },
+};
