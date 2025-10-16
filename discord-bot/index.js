98504733f52d0232866ec9397b7adde49d524f12
Kristian Cybul <kcybul123@gmail.com>
2025-10-16 11:14:15 -0500

    First upload of the bot files

---
+++ discord-bot/index.js
@@ -0,0 +1,74 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Collection } = require('discord.js');
+const { MessageFlags } = require('discord.js');
+
+// Debug script
+console.log('Current working directory:', process.cwd());
+
+// Require the necessary classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+// Marks token what is in the config file
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds ]});
+
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+
+for (const folder of commandFolders) {
+	const commandPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandPath).filter((file) => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandPath, file);
+		const command = require(filePath);
+
+		// Set a new item in the Collection with the key as the command nameand the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		}
+		else{
+			console.log(`[WARNING] The command at ${filepath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName)
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({
+				content: 'There was an error while executing this command!',
+				ephemeral: true,
+			});
+		} else {
+			await interaction.reply({
+				content: 'There was an error while executing this command!',
+				ephemeral: true,
+			});
+		}
+	}
+});
+
+// Log in to Discord with your client's token
+client.login(token);
