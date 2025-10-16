6f29c1264db342a10d197495f402eb81d2ebc8ee
Sirbumpkis <boonemcconathy7@gmail.com>
2025-10-16 12:50:57 -0400

    Updated help.js

--- discord-bot/commands/utility/help.js
+++ discord-bot/commands/utility/help.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder} = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
         .setname('dev')
-        .setdescription('Who to contact for help?'),
+        .setDescription('Who to contact for help?'),
   async execue(interaction) {
     await interation.reply('Please Contact @Blue and or @SlipperyBooney :P);
                            },
